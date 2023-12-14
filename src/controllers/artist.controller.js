9363d7d7c29f2ab4bc9be5f319af2670a090b6c1
RyanPeyrot <ryan.peyrot@gmail.com>
2023-12-14 12:44:19 +0100

    Ajout d'une thumbnail pour les artists

--- src/controllers/artist.controller.js
+++ src/controllers/artist.controller.js
@@ -1,12 +1,38 @@
 const Artist = require("../models/artist.model")
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const s3 = new AWS.S3();
+const cloudfront = 'https://d2be9zb8yn0dxh.cloudfront.net/';
+
+const uploadS3 = (params) => {
+  return s3.upload(params).promise();
+};
 
 exports.createOne = async (req,res) => {
+  let uploadedFileName
+  if (req.file) {
+      const uploadMediaParams = {
+        Bucket: 'spotifake-ral',
+        Key: `artist/thumbnail_${req.file.originalname}`,
+        Body: fs.createReadStream(req.file.path),
+      };
+
+      await uploadS3(uploadMediaParams)
+        .then((data) => {
+          uploadedFileName = cloudfront+data.Key;
+        })
+        .catch((err) => {
+          console.error('Erreur lors du téléchargement:', err);
+          res.status(500).json({ message: 'Erreur lors du téléchargement du fichier' });
+        });
+  }
+
   try{
     const newArtist = new Artist({
       name : req.body.name,
       albums: req.body.albums,
       titles: req.body.titles,
-      thumbnail: req.body.thumbnail != undefined ? req.body.thumbnail : 'https://d2be9zb8yn0dxh.cloudfront.net/'
+      thumbnail: req.file !== undefined ? uploadedFileName : cloudfront
     });
 
     const artists = await newArtist.save();
@@ -87,6 +113,43 @@ exports.updateOne = async (req, res) => {
     return res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'artiste' });
   }
 };
+
+exports.updateThumbnail = async (req, res) => {
+  try {
+    if (req.file) {
+      const uploadMediaParams = {
+        Bucket: 'spotifake-ral',
+        Key: `artist/thumbnail_${req.file.originalname}`,
+        Body: fs.createReadStream(req.file.path),
+      };
+
+      await uploadS3(uploadMediaParams)
+        .then(async (data) => {
+
+          const updatedArtist = await Artist.findOneAndUpdate(
+            {_id: req.params.id},
+            {thumbnail: cloudfront + data.Key},
+            {new: true} // Pour renvoyer le document mis à jour
+          );
+
+          if (updatedArtist) {
+            return res.status(200).json(updatedArtist);
+          } else {
+            return res.status(404).json({message: "Aucun artist trouvé"});
+          }
+        })
+        .catch((err) => {
+          console.error('Erreur lors du téléchargement:', err);
+          res.status(500).json({ message: 'Erreur lors du téléchargement du fichier' });
+        });
+    } else {
+      return res.status(500).json({message:"Aucun fichier transmis"})
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Erreur lors de la mise à jour de l\'artiste' });
+  }
+}
 
 exports.deleteOne = async (req,res) => {
   try {
