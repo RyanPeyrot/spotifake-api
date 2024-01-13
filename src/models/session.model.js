f5a82566f158b5a7d753f082fdd67fac86495b30
Ryan "NayS" PEYROT <60643765+RyanPeyrot@users.noreply.github.com>
2024-01-13 13:38:37 +0100

    Update session.model.js
    Update pour le partage de musique

--- src/models/session.model.js
+++ src/models/session.model.js
@@ -4,7 +4,12 @@ const {Schema} = require("mongoose");
 const sessionSchema = mongoose.Schema({
   users : [{
     type: String,
-  }]
+  }],
+  currentMedia: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'Media',
+    default:null
+  }
 })
 
 module.exports = mongoose.model('Session', sessionSchema);
