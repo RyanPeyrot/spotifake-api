ff0d6b53ad3e330666cc2de757218006769a0aa4
RyanPeyrot <ryan.peyrot@gmail.com>
2024-01-19 11:46:21 +0100

    websocket Correction

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ const app = express();
 const apiRouter = require('./routes/index');
 const cors = require('cors');
 const aws = require("aws-sdk");
-import { createServer } from "http";
-import { Server } from "socket.io";
+const http = require('http');
+const socketIo = require('socket.io');
+const server = http.createServer(app);
+const io = socketIo(server);
 
 //const logger = require('./utils/logger')
 require('dotenv').config();
@@ -18,46 +20,42 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-const httpServer = createServer();
-
-// Initialisation du serveur socket.io sur le serveur HTTP.
-// Configuration pour autoriser les requêtes CORS depuis localhost:3000.
-const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
-});
-
-// Un objet Map pour stocker les utilisateurs connectés.
-const users = new Map();
-
-// Événement déclenché lorsqu'un nouveau client se connecte.
 io.on('connection', (socket) => {
+    console.log('Un utilisateur est connecté');
+
+    socket.on('updateMedia', async (sessionId, mediaId) => {
+        try {
+            // Mettre à jour la session avec le nouveau média
+            const updatedSession = await Session.findByIdAndUpdate(
+                sessionId,
+                { currentMedia: mediaId},
+                { new: true }
+            );
 
 
-    // Lorsqu'un client envoie son nom d'utilisateur après la connexion.
-    socket.on('register', (username) => {
-        // Stocker le nom d'utilisateur avec l'ID du socket correspondant.
-        users.set(socket.id, username);
-        console.log(`${username} s'est connecté`);
+            if (!updatedSession) {
+                // Gérer le cas où la session n'est pas trouvée
+                socket.emit('updateError', 'Session non trouvée.');
+                return;
+            }
+
+            // Notifier tous les clients connectés à cette session
+            io.to(sessionId) .emit ('mediaUpdated', updatedSession);
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour du média :', error);
+            socket.emit('updateError', 'Erreur lors de la mise à jour du média')
+        }
     });
 
-    // Lorsqu'un client se déconnecte.
     socket.on('disconnect', () => {
-        // Récupérer le nom d'utilisateur à partir de l'ID du socket, ou utiliser 'Anonyme'.
-        const username = users.get(socket.id) || 'Anonyme';
-        console.log(`${username} s'est déconnecté`);
-
-        // Supprimer l'utilisateur de la liste.
-        users.delete(socket.id);
-    });
-
-    // Lorsqu'un client partage une musique.
-    socket.on('shareTime', (musique) => {
-        // Diffuser la musique à tous les clients connectés.
-        io.emit('shareTime', musique);
+        console.log('Un utilisateur est déconnecté');
     });
 });
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Serveur à l'écoute sur le port ${PORT}`));
+
+
 mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@${process.env.DBCLUSTER}.vfjzeo9.mongodb.net/spotifakedb2?retryWrites=true&w=majority
 `).then(()=>{
     console.log("Connection successfull");
